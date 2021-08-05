e8bb119c9a8315da1c2f3697f75de721dccc1558
student <rans.subdivo@gmail.com>
2021-08-05 18:07:29 +1000

    added axios + API call

--- src/screens/Home.jsx
+++ src/screens/Home.jsx
@@ -1,5 +1,7 @@
 import React, { useState, useEffect, useLayoutEffect } from "react";
 import { StyleSheet, Text, View, Button, TouchableOpacity } from "react-native";
+
+import axios from "axios";
 // name import
 import { ROUTES } from "../utils/constants";
 import { MaterialIcons } from "@expo/vector-icons";
@@ -8,14 +10,17 @@ import * as Location from 'expo-location';
 // default import
 import AddLocation from "../components/Home/AddLocation";
 import CitiesList from "../components/Home/CitiesList";
+import API_CALL from "../utils/clientSecrets/openWeather";
+
 
 const Home = ({ navigation }) => {
   // states for this component
 
   const [searchResult, setSearchResult] = useState();
   const [currentPosition, setCurrentPosition] = useState();
-
-
+  const [location, setLocation] = useState(null);
+  const [currentCity,setCurrentCity] = useState();
+  
   // hook call for this component
   useEffect(() => {
     console.log("Home Page Screen");
@@ -51,10 +56,42 @@ const Home = ({ navigation }) => {
       setErrorMsg('Premission to access location denied')
     }
     console.log('requestForegroundPermissionsAsync status', status);
-    let location = await Location.getCurrentPositionAsync({});
+    //get the current location 
+    let location = await Location.getCurrentPositionAsync({accuracy:Location.Accuracy.Highest});
+    // let location = await Location.getCurrentPositionAsync({accuracy:Location.Accuracy.Balanced}).then(()=>{
+    //   setLocation(location);
+    // });
 
-    setCurrentPosition(location);
-    console.log('CurrentLocation:',location);
+    //setCurrentPosition(location);
+    console.log('Home screen check  - CurrentLocation:',location);
+
+    const {latitude,longitude}=location.coords;
+    console.log('latitude: ',latitude);
+    console.log('longitude: ',longitude);
+
+
+    //if we got the coordinates we need, we can proceed to fetching the coordinates
+    if(latitude&&longitude){
+      const URL = `${API_CALL.BASE_URL}lat=${latitude}&lon=${longitude}${API_CALL.UNIT}${API_CALL.KEY}`;
+      axios.get(URL)
+        .then((response) => {
+          console.log("response: ", response.data);
+          let name = response.data.name;
+          let id = response.data.id;
+          console.log("name: ", name);
+          console.log("id: ", id);
+          setCurrentCity({name,id});
+        })
+        .catch((error) => {
+          console.log('error @axios.get(): ', error);
+          
+        })
+        .finally(() => {
+          console.log('axios.get finally');
+
+        });
+    }
+
     })();
   },[])
 
@@ -70,7 +107,9 @@ const Home = ({ navigation }) => {
         onPress={() => navigation.navigate(ROUTES.PROFILE)}
         title="Profile Page"
       />
+      
     </View>
+
   );
 };
 
